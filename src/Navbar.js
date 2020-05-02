c75355bb64ccc5250a9c8e82eda30863d7f96c6b
gotoflorian <gotoflorian.pro@gmail.com>
2020-05-02 21:55:52 +0200

    add more styles to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
+import colors from "./colors";
 
-const Navbar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt A Pet</Link>
-    <span aria-label="logo" role="img">
-      🦁
-    </span>
-  </header>
-);
+const Navbar = () => {
+  // const [color, setColor] = useState("green");
+  const [padding, setPadding] = useState(15);
+
+  return (
+    <header
+      css={css`
+        background-color: ${colors.primary};
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt A Pet</Link>
+      <span
+        css={css`
+          font-size: 60px;
+          &:hover {
+            text-decoration: underline;
+          }
+        `}
+        aria-label="logo"
+        role="img"
+      >
+        🦁
+      </span>
+    </header>
+  );
+};
 
 export default Navbar;
