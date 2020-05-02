ba49819d8fc611728074e38f17bf13150209db28
gotoflorian <gotoflorian.pro@gmail.com>
2020-05-02 22:05:02 +0200

    add more styles to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
 import colors from "./colors";
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const Navbar = () => {
   // const [color, setColor] = useState("green");
-  const [padding, setPadding] = useState(15);
+  const [padding] = useState(15);
 
   return (
     <header
       css={css`
-        background-color: ${colors.primary};
+        background-color: ${colors.secondary};
         padding: ${padding}px;
       `}
     >
@@ -18,7 +24,11 @@ const Navbar = () => {
       <span
         css={css`
           font-size: 60px;
+          display: inline-block;
+          animation: 10s ${spin} linear infinite;
+
           &:hover {
+            animation: 1s ${spin} linear infinite reverse;
             text-decoration: underline;
           }
         `}
